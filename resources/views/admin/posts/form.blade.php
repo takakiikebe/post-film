d4fb6d5fab1dd0102386a8a9b883f56b8561eec6
takaki Ikebe <takakiikebe@me.com>
2020-08-15 17:25:42 +0900

    Update form.blade.php

--- resources/views/admin/posts/form.blade.php
+++ resources/views/admin/posts/form.blade.php
@@ -20,19 +20,17 @@
 </select>
         {!! $errors->first('category', '<p class="help-block">:message</p>') !!}
     </div>
-</div>
-<div class="form-group {{ $errors->has('url1') ? 'has-error' : ''}}">
-    <label for="url1" class="col-md-4 control-label">{{ 'Url1' }}</label>
+</div><div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+    <label for="title" class="col-md-4 control-label">{{ 'Title' }}</label>
     <div class="col-md-6">
-        <textarea class="form-control" rows="5" name="url1" type="textarea" id="url1" >{{ $post->url1 or ''}}</textarea>
-        {!! $errors->first('url1', '<p class="help-block">:message</p>') !!}
+        <input class="form-control" name="title" type="text" id="title" value="{{ $post->title or ''}}" >
+        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
     </div>
-</div>
-<div class="form-group {{ $errors->has('url2') ? 'has-error' : ''}}">
-    <label for="url2" class="col-md-4 control-label">{{ 'Url2' }}</label>
+</div><div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+    <label for="title" class="col-md-4 control-label">{{ 'Title' }}</label>
     <div class="col-md-6">
-        <textarea class="form-control" rows="5" name="url2" type="textarea" id="url2" >{{ $post->url2 or ''}}</textarea>
-        {!! $errors->first('url2', '<p class="help-block">:message</p>') !!}
+        <input class="form-control" name="title" type="text" id="title" value="{{ $post->title or ''}}" >
+        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
     </div>
 </div>
 <div class="form-group">
