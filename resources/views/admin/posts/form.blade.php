51a5818f45ca519be99e695ed3f1a1b07dd667b8
takaki Ikebe <takakiikebe@me.com>
2020-08-15 17:28:59 +0900

    Update form.blade.php

--- resources/views/admin/posts/form.blade.php
+++ resources/views/admin/posts/form.blade.php
@@ -20,18 +20,6 @@
 </select>
         {!! $errors->first('category', '<p class="help-block">:message</p>') !!}
     </div>
-</div><div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
-    <label for="title" class="col-md-4 control-label">{{ 'Title' }}</label>
-    <div class="col-md-6">
-        <input class="form-control" name="title" type="text" id="title" value="{{ $post->title or ''}}" >
-        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
-    </div>
-</div><div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
-    <label for="title" class="col-md-4 control-label">{{ 'Title' }}</label>
-    <div class="col-md-6">
-        <input class="form-control" name="title" type="text" id="title" value="{{ $post->title or ''}}" >
-        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
-    </div>
 </div>
 <div class="form-group">
     <div class="col-md-offset-4 col-md-4">
