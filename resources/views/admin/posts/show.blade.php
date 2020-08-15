fb2b85667890432aa931f55e70779110e9d49b62
takaki Ikebe <takakiikebe@me.com>
2020-08-15 20:30:42 +0900

    Update show.blade.php

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -28,17 +28,16 @@
                                         <th>ID</th><td>{{ $post->id }}</td>
                                     </tr>
                                     <tr><th> Title </th><td> {{ $post->title }} </td></tr><tr><th> Content </th><td> {{ $post->content }} </td></tr><tr><th> Category </th><td> {{ $post->category }} </td></tr><tr><th> Url1 </th><td>
-                                            <form action="{{ url('site/'.$post->url1) }}">
+                                            <form action="{{ url($post->url1) }}">
                                                 <button type="submit" class="btn btn-primary">
                                                     <i class="fa fa-btn"></i>100
                                                 </button>
                                             </form>
-                                        {{ $post->url1 }} </td></tr><tr><th> Url2 </th><td><form action="{{ url('site/'.$post->url2) }}">
+                                        {{ $post->url1 }} </td></tr><tr><th> Url2 </th><td><form action="{{ url($post->url2) }}">
                                                 <button type="submit" class="btn btn-primary">
                                                     <i class="fa fa-btn"></i>100
                                                 </button>
-                                            </form>
- {{ $post->url2 }} </td></tr>
+                                            </form> {{ $post->url2 }} </td></tr>
                                 </tbody>
                             </table>
                         </div>
