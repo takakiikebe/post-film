bf693f4ceb38268aa39b1149ef38e3ecb9231453
takaki Ikebe <takakiikebe@me.com>
2020-08-15 16:49:40 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Recommend-List
                 </div>
 
                 <div class="links">
