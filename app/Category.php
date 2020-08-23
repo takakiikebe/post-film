2a0868bfe4a29aa8ee762c6a732a939f732b41d3
takaki Ikebe <takakiikebe@me.com>
2020-08-23 10:34:55 +0900

    Create Category.php

---
+++ app/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $fillable = [
+    'id', 'name', 'namejapa', 'created_at',
+];
+}
